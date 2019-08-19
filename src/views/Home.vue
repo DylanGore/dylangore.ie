aa12028b38bb8c2dac35af04fd59723e546aa209
Dylan Gore <hello@dylangore.ie>
2019-08-19 16:47:43 +0100

    Initial commit of Vue code

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <v-container>
+            <v-layout wrap>
+                <v-flex sm12>
+                    <h2>Home</h2>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home'
+};
+</script>
