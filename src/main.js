5a9964d7c9deb597048f97ce1f006845fd0c2bba
Dylan Gore <hello@dylangore.ie>
2019-08-30 20:13:56 +0100

    Added Google Analytics

--- src/main.js
+++ src/main.js
@@ -2,16 +2,31 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-//Firebase
 import { auth } from '@/firebase/init';
 import { firestorePlugin } from 'vuefire';
-
-Vue.use(firestorePlugin);
-
-// Layouts
+import VueAnalytics from 'vue-analytics';
 import Default from './layouts/Default';
 import Cover from './layouts/Cover';
+
+//Firebase
+Vue.use(firestorePlugin);
+
+// Google Analytics
+const isProd = process.env.NODE_ENV === 'production';
+
+Vue.use(VueAnalytics, {
+    id: process.env.VUE_APP_GA_TRACKING_ID,
+    router,
+    debug: {
+        disabled: !isProd,
+        sendHitTask: isProd
+    },
+    beforeFirstHit() {
+        Vue.$ga.set('anonymizeIp', true);
+    }
+});
+
+// Layouts
 Vue.component('default-layout', Default);
 Vue.component('cover-layout', Cover);
 
